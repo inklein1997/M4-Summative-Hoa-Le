299cda39166aec5e5fa8dc796c3c70680ecd9eaf
Michael Klein <Michaelklein1997@gmail.com>
2022-05-14 12:07:03 -0500

    created table for Game using ORM

--- Game-Store/src/main/java/com/company/GameStore/DTO/Game.java
+++ Game-Store/src/main/java/com/company/GameStore/DTO/Game.java
@@ -1,5 +1,141 @@
 package com.company.GameStore.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@Table(name = "game")
 public class Game {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    private Integer game_id;
+    @NotNull
+    @Size(max = 50)
+    private String title;
+    @Column(name = "esrb_rating")
+    @NotNull
+    @Size(max = 50)
+    private String esrbRating;
+    @NotNull
+    @Size(max = 255)
+    private String description;
+    @NotNull
+    @Digits(integer = 5, fraction = 2)
+    private double price;
+    @NotNull
+    @Size(max = 50)
+    private String studio;
+    @NotNull
+    private int quantity;
+
+    public Game(Integer game_id, String title, String esrbRating, String description, double price, String studio, int quantity) {
+        this.game_id = game_id;
+        this.title = title;
+        this.esrbRating = esrbRating;
+        this.description = description;
+        this.price = price;
+        this.studio = studio;
+        this.quantity = quantity;
+    }
+
+    public Game(String title, String esrbRating, String description, double price, String studio, int quantity) {
+        this.title = title;
+        this.esrbRating = esrbRating;
+        this.description = description;
+        this.price = price;
+        this.studio = studio;
+        this.quantity = quantity;
+    }
+
+    public Game() {
+    }
+
+    public Integer getGame_id() {
+        return game_id;
+    }
+
+    public void setGame_id(Integer game_id) {
+        this.game_id = game_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEsrbRating() {
+        return esrbRating;
+    }
+
+    public void setEsrbRating(String esrbRating) {
+        this.esrbRating = esrbRating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Double.compare(game.price, price) == 0 && quantity == game.quantity && Objects.equals(game_id, game.game_id) && Objects.equals(title, game.title) && Objects.equals(esrbRating, game.esrbRating) && Objects.equals(description, game.description) && Objects.equals(studio, game.studio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game_id, title, esrbRating, description, price, studio, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "game_id=" + game_id +
+                ", title='" + title + '\'' +
+                ", esrbRating='" + esrbRating + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", studio='" + studio + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
