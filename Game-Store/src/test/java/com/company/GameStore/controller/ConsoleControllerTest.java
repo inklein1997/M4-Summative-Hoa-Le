d344f975e67e3cefff6c74cff8d2fdc5f6d37557
Honorine Ndom <honorinendzah@gmail.com>
2022-05-16 10:31:01 -0700

    added consolecontrollertest

---
+++ Game-Store/src/test/java/com/company/GameStore/controller/ConsoleControllerTest.java
@@ -0,0 +1,25 @@
+package com.company.GameStore.controller;
+
+
+import com.company.GameStore.DTO.Console;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ConsoleController.class)
+public class ConsoleControllerTest {
+
+
+}
