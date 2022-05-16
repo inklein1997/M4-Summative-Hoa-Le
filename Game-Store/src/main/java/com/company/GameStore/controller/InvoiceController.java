b5e29884eac444585291a99c6ea823aebfc7d9a5
Michael Klein <Michaelklein1997@gmail.com>
2022-05-16 14:25:39 -0500

    created initial routes for invoice

--- Game-Store/src/main/java/com/company/GameStore/controller/InvoiceController.java
+++ Game-Store/src/main/java/com/company/GameStore/controller/InvoiceController.java
@@ -1,8 +1,36 @@
 package com.company.GameStore.controller;
 
+import com.company.GameStore.DTO.Invoice;
+import com.company.GameStore.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class InvoiceController {
+
+    @Autowired
+    ServiceLayer serviceLayer;
+
+    @GetMapping("/invoices")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Invoice> getAllInvoices() {
+        return serviceLayer.getAllInvoices();
+    }
+
+    @GetMapping("/invoices/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Invoice> getSingleInvoice(@PathVariable int id) {
+        return serviceLayer.getInvoiceById(id);
+    }
+
+    @PostMapping("/invoices")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Invoice createInvoice(@RequestBody Invoice invoice) {
+        return serviceLayer.createInvoice(invoice);
+    }
+
 }
