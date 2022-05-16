8fab3cff37ca3154f374d54354105bd935bcf968
Honorine Ndom <honorinendzah@gmail.com>
2022-05-16 12:15:18 -0700

    added route test for get all

--- Game-Store/src/test/java/com/company/GameStore/controller/ConsoleControllerTest.java
+++ Game-Store/src/test/java/com/company/GameStore/controller/ConsoleControllerTest.java
@@ -21,5 +21,43 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(ConsoleController.class)
 public class ConsoleControllerTest {
 
+    // Wiring in the MockMvc object
+    @Autowired
+    private MockMvc mockMvc;
+
+    // ObjectMapper used to convert Java objects to JSON and vice versa
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Before
+    public void setUp() {
+        // This is the standard set up method that runs before each test. It's typically used for instantiating test
+        // objects. We don't have to do anything special for mockMvc since it's Autowired. We will however be using
+        // setUp() in the future.
+    }
+
+
+    // testing GET record/{id}
+    @Test
+    public void shouldReturnAllConsoles() throws Exception {
+
+        // ARRANGE
+        Consoles outputConsoles = new Consoles();
+        outputConsoles.setId();
+        outputConsoles.setModel();
+        outputConsoles.setManufacturer();
+        outputConsoles.setMemoryAmount();
+        outputConsoles.setProcessor();
+        outputConsoles.setPrice();
+        outputConsoles.setQuantity();
+
+        String outputJson = mapper.writeValueAsString(outputConsoles);
+
+        // ACT
+        mockMvc.perform(get("/consoles/1"))
+                .andDo(print())
+                .andExpect(status().isOk())                     // ASSERT that we got back 200 OK.
+                .andExpect(content().json(outputJson));         // ASSERT that what we're expecting is what we got back.
+    }
+
 
 }
